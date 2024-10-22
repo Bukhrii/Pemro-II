b78e5ec4117e1e034d9f3151de605ab9eb3b5200
Bukhrii <bukharihari123@gmail.com>
2024-10-22 19:21:29 +0800

    .

--- modul2/src/PRAK202_2310817210015_MuhammadBukhariFitri/Soal2Main.java
+++ modul2/src/PRAK202_2310817210015_MuhammadBukhariFitri/Soal2Main.java
@@ -1,5 +1,30 @@
 package PRAK202_2310817210015_MuhammadBukhariFitri;
 
+class Kopi {
+	String namaKopi, ukuran, nama;
+	double harga;
+	final double pajak = 0.11;
+	
+	void info() {
+		 System.out.println("Nama Kopi: " + namaKopi);
+		 System.out.println("Ukuran: " + ukuran);
+		 System.out.println("Harga: " + harga);
+	}
+	
+	void setPembeli(String nama) {
+		this.nama = nama;
+	}
+	
+	String getPembeli() {
+		return nama;
+	}
+	
+	double getPajak() {
+		return pajak * harga;
+	}
+
+}
+
 public class Soal2Main {
 		public static void main(String[] args) {
 			Kopi kopi1 = new Kopi();
